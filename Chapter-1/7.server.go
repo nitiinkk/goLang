76bf7b7b503253f2581e044481a49a93243d17d4
nitiiink1 <nitiiink1@gmail.com>
2023-10-08 20:31:33 +0530

    simple server

---
+++ Chapter-1/7.server.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.path = %q\n", r.URL.Path)
+}
+
+//input: go run 7.server.go &
+//output: URL.path = "/hello1"
